3a2993f64db2d7a1df297d017595266d3dce826e
Gavi Schneider <gavischneider@gmail.com>
2020-10-21 21:24:51 +0300

    test

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -19,6 +19,6 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(205)//200
-      .expect('Hello World!');
+      .expect('Hell');//'Hello World!'
   });
 });
